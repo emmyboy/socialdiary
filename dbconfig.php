188b4147f1b42997981ca9792016d5520d38bee0
Emeka <emmyboy@gmail.com>
2015-04-26 15:19:40 -0700

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -1,6 +1,6 @@
 <?php
 define("DB_HOST", "dbserver.engr.scu.edu:3306");
 define("DB_USER", "cegwuatu");
-define("DB_PASSWORD", "00000756216");
+define("DB_PASSWORD", "00000xxxxxx");
 define("DB_NAME", "sdb_cegwuatu");
 ?>
